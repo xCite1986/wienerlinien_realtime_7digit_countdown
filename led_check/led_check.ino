a769b0c5dc94193f0e4b40e837f3bb4ca0cd35c0
Alex Prohaska <alexander.prohaska@oebb.at>
2025-10-01 12:20:27 +0200

    add led check ino

---
+++ led_check/led_check.ino
@@ -0,0 +1,46 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN            D4          // D4 = GPIO2 auf dem Wemos D1 Pro/Mini
+#define NUMPIXELS      58          // Anzahl der NeoPixel
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  pixels.begin(); // Initialisierung der NeoPixel
+  pixels.clear(); // Alle Pixel aus
+  pixels.show();
+}
+
+void loop() {
+  // Test 1: Alle nacheinander rot, grün, blau durchgehen
+  for (uint16_t i=0; i<NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(255,0,0)); // Rot
+    pixels.show();
+    delay(50);
+  }
+  delay(300);
+  for (uint16_t i=0; i<NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(0,255,0)); // Grün
+    pixels.show();
+    delay(50);
+  }
+  delay(300);
+  for (uint16_t i=0; i<NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(0,0,255)); // Blau
+    pixels.show();
+    delay(50);
+  }
+  delay(300);
+
+  // Test 2: Alle gleichzeitig weiß
+  for (uint16_t i=0; i<NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(255,255,255));
+  }
+  pixels.show();
+  delay(1000);
+
+  // Ausmachen
+  pixels.clear();
+  pixels.show();
+  delay(1000);
+}
